678c0fc2d9017f8b9417d6ba44cadc9d073b4826
stopyc <e13242597082@outlook.com>
2023-09-11 20:39:56 +0800

    feat：完成文件处理工具类的编写

---
+++ PaperCheck/src/main/java/shop/utils/PaperFileUtil.java
@@ -0,0 +1,21 @@
+package shop.utils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+/**
+ * @author stopyc
+ * （论文）文件处理工厂
+ */
+public class PaperFileUtil {
+    public static String getStringFromFile(String path) throws IOException {
+        List<String> lines = Files.readAllLines(Paths.get(path));
+        //合成为一个String
+        return String.join("\n", lines);
+    }
+    public static void write(String path, List<String> str) throws IOException {
+        Files.write(Paths.get(path), str);
+    }
+}
